64f2f59078e749746887800cc46d1cfcf426f2e3
康明 浅野 <yasuaki199181@outlook.com>
2023-08-22 01:12:03 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleConsoleApp/ChatGPT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    public class ChatGPT
+    {
+        public string? apiKey { get; set; }
+        public string? resmessage { get; set; }
+
+        public ChatGPT()
+        {
+            this.apiKey = Environment.GetEnvironmentVariable("ChatGPT_APIKey");
+        }
+
+        public async Task getChatGPTResponse(string reqmessage)
+        {
+            var api = new OpenAI_API.OpenAIAPI(this.apiKey);
+            var chat = api.Chat.CreateConversation();
+
+            chat.AppendUserInput(reqmessage);
+            resmessage = await chat.GetResponseFromChatbotAsync();
+        }
+    }
+}
---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using SampleConsoleApp;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            var chatgpt = new ChatGPT();
+            var input = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                await chatgpt.getChatGPTResponse(input);
+                Console.WriteLine(chatgpt.resmessage);
+            }     
+        }
+
+        catch(Exception e)
+        {
+            Console.WriteLine($"Error:{e.Message}");
+        }        
+    }
+}
